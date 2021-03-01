6d0f17cc4e97b5021b0a5e05056e7682029336a8
Cezar Halmagean <cezar@mixandgo.com>
2021-03-01 13:50:55 +0200

    Initial commit

---
+++ features/homepage.feature
@@ -0,0 +1,5 @@
+Feature: Homepage
+
+  Scenario: I can see a welcome message
+    Given I am on the homepage
+    Then I should see a welcome message
---
+++ features/step_definitions/homepage_steps.rb
@@ -0,0 +1,7 @@
+Given('I am on the homepage') do
+  visit "/"
+end
+
+Then('I should see a welcome message') do
+  expect(page).to have_content("Welcome")
+end
